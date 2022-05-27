19ee4d2d8cb68feed7e423901a8ba61c7bd6b0de
JaspreetKaur09 <91208959+JaspreetKaur09@users.noreply.github.com>
2022-05-27 08:47:05 -0400

    add the code to calculate Tax

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 /*
 	WEB 303 Assignment 1 - jQuery
-	{Your Name Here}
+	Jaspreet Kaur
 */
+console.log('jass');
+var $price = $('#price');
+
+$price.change(function(){
+    var $taxAmount = $price * (13/100);
+	$('span#tax').replacewith($taxAmount);
+	$taxAmount.toFixed(2);
+
+})
 
