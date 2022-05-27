20aca006b70229544a3c1470f94356c45079dde5
JaspreetKaur09 <91208959+JaspreetKaur09@users.noreply.github.com>
2022-05-27 11:00:17 -0400

    Update the code  .

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 /*
 	WEB 303 Assignment 1 - jQuery
-	{Your Name Here}
+	Jaspreet Kaur
 */
+
+
+	$("#price").change(function(){
+		var price = $("#price").val();
+		var taxAmount = price + 0.13;
+		$("#tax").text("$" + taxAmount.tofixed(2));
+	});
+
+	$("#add-price").click(function(){
+		console.log("jass");
+		$("#price-list").text("$" + taxAmount);
+	});
+	
+
+          
+
+
+
+
+
+
 
