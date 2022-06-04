2ae455716ecc4ddcbfdcabc438052807fbfd58b4
Jaspreet Kaur <91208959+JaspreetKaur09@users.noreply.github.com>
2022-06-03 22:07:13 -0400

    Update script.js
    update the code .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
 // WEB303 Assignment 2
-// Your Name Here
+// Jaspreet Kaur
 
+
+$(function () {
+    $("#content-wrapper").on("click", function () {
+      makeXHRCallForJSON();
+    });
+  });
+
+  function makeXHRCallForJSON(){
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', "prospect.html");
+  
+   
+  xhr.onload = function (){
+      if (xhr.status === 200){
+        let elm = document.getElementById("a#prospect");
+        elm.innerHTML = xhr.responseText;
+          
+      }
+  };
+  xhr.send();
+}
+
+function makeXHRCallForJSON(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', "convert.html");
+
+ 
+xhr.onload = function (){
+    if (xhr.status === 200){
+      let elm = document.getElementById("a#convert");
+      elm.innerHTML = xhr.responseText;
+        
+    }
+};
+xhr.send();
+}
+
+function makeXHRCallForJSON(){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', "retain.html");
+
+ 
+xhr.onload = function (){
+    if (xhr.status === 200){
+      let elm = document.getElementById("a#retain");
+      elm.innerHTML = xhr.responseText;
+        
+    }
+};
+xhr.send();
+}
