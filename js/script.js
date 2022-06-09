49ae91a3828ccd25f2c8c516cb0d42b715d012d8
JaspreetKaur09 <91208959+JaspreetKaur09@users.noreply.github.com>
2022-06-09 12:37:19 -0400

    update the script.js file

--- js/script.js
+++ js/script.js
@@ -1,2 +1,31 @@
 // WEB303 Assignment 3
-// Your Name Here
+// Jaspreet Kaur
+
+$.getJSON("data/glitchTeam.json", function (JSONDataFromFile, statusText, jqXHRObject){
+    $.each(JSONDataFromFile.members, function(i, val){
+     
+        let nameObject = $(`<h4 class="getJSONRequest">${val.fullname}</h4>`);
+        let positionObject = $(`<h6 class="getJSONRequest">${val.position}</h6>`);
+        let shortbioObject = $(`<pclass="getJSONRequest">${val.shortbio}</p>`);
+        $("div#glitch-team")
+        .append(nameObject)
+        .append(positionObject)
+        .append(shortbioObject);
+    });
+});
+
+$.ajax({
+    url: "glitchTeam.json",
+
+    beforeSend: function (){
+           let loadText = $("<h2></h2>").text("Loading team...");
+           $(`div#glitch-team`).append(loadText);
+    },
+    error:function(){
+        
+    }
+
+    complete: function (){
+
+    }
+})
