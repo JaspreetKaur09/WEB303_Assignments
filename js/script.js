a356f46bca70758eafa694f81ef39a1d57140ea0
Jaspreet Kaur <91208959+JaspreetKaur09@users.noreply.github.com>
2022-07-31 20:19:36 -0400

    add the code for search box,

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-// your code goes here
+// Jaspreet Kaur 0784780
+
+$(document).ready(function () {
+    $("dearch").on("keyup", function (){
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function () {
+            if (value == "") {
+              $("#myTable tr").removeClass("highlight");
+            } else $(this).toggleClass("highlight", $(this).text().toLowerCase().indexOf(value) != -1);
+        });
+    });
+
+});
